8f43f1034253a857b5cab6edccc8df10673cab14
HY31 <qtghdus@gmail.com>
2023-08-18 16:53:31 +0900

    firstCommit
    test

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleRPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("콘솔 RPG의 세계에 오신 것을 환영합니다.");
+        }
+    }
+}
